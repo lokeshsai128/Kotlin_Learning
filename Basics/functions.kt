8533c68b610b719b96963a4af1c98e2dd299acec
lokeshsai128 <lokeshsai128@gmail.com>
2025-06-11 10:21:01 +0530

    functions

---
+++ Basics/functions.kt
@@ -0,0 +1,7 @@
+// Standard library function which are pre-defined functions like print(),sqrt()
+// User defined functions are created by the developer to perform a specific task, The signature would look something like this : 
+// fun_keyword fun_name (Paremeter_list:datatype) : return type {}
+// we can also use one liner functions if it is an expression
+// a function must be called within the main fucntion
+// different ways of calling a function are : 
+// There are three ways of calling a function
