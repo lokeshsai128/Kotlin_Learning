60516e3f253b56a4934b7238461e98273d300bbe
lokeshsai128 <lokeshsai128@gmail.com>
2025-06-11 10:36:50 +0530

    commit

--- Basics/functions.kt
+++ Basics/functions.kt
@@ -11,6 +11,6 @@
 fun main(){
     myFun("I am IronMan","Marvel")
 }
-fun myFun(message:string, name:string){
-    println("Message : $message for  $name")
+fun myFun(message:String, name:String){
+    println("Message : $message ,conveying to : $name")
 }
