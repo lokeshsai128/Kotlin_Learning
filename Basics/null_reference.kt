d2ffd0edf0012adbc40f78b388066f814eb042ff
lokeshsai128 <lokeshsai128@gmail.com>
2025-06-12 11:46:45 +0530

    push

--- Basics/null_reference.kt
+++ Basics/null_reference.kt
@@ -3,10 +3,19 @@
 // functions/methods of nullable references cannot be called directly
 // instead we can use a safe null operator "?." which assigns a null value if there is no value
 // we can also assign some define value using safe Elvis operator "?:" instead of null
-// we also have a non null assertin operator which actually converts an object irrespective of it's type to non null , we use this only in the case when it cannot be null at all  
+// we also have a non null assertin operator which actually converts an object irrespective of it's type to non null , we use this only in the case when it should not be null at all and if it gets null then it throws a NPE
+
+// fun main(){
+//     var nullablereference : String? = "I can take null"
+//     println(nullablereference)
+//     nullablereference = null
+//     println(nullablereference)
+// }
+
 fun main(){
-    var nullablereference : String? = "I can take null"
-    println(nullablereference)
-    nullablereference = null
-    println(nullablereference)
+    var str : String? = null
+    println(" The safe operator "+ str?.length)
+    var str_length : Int = str?.length ?: 0
+    println(" The Elvis operator "+ str_length)
+    println(" The non null operator "+ str!!.length)
 }
