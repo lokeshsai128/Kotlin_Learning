1368d83e2cf1222eb533b22b145101e963c00b83
lokeshsai128 <lokeshsai128@gmail.com>
2025-06-12 11:30:42 +0530

    push

--- Basics/null_reference.kt
+++ Basics/null_reference.kt
@@ -1,2 +1,8 @@
 // cannot assign null values by default
 //  "?" will be used beside the datatype to define it as nullable
+fun main(){
+    var nullablereference : String? = "I can take null"
+    println(nullablereference)
+    nullablereference = null
+    println(nullablereference)
+}
